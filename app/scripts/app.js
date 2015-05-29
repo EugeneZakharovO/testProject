b2b4fab35b2d9ffd2107de510426bc4bbf985da6
EugeneZakharov <evgeniykpi@gmail.com>
2015-05-29 13:40:06 +0300

    updates

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -23,5 +23,6 @@ angular
       }
     ])
     .run([
-      function () {}
+      function () {
+      }
     ]);
--- app/scripts/controllers/allContacts.js
+++ app/scripts/controllers/allContacts.js
@@ -1,6 +1,32 @@
 'use strict';
+var AllContactsController = BaseController.extend({
+
+  init:function(scope){
+    this._super(scope);
+    this.getContact()
+  },
+
+  defineListeners:function(){
+  },
+
+  defineScope:function(){
+    this.$scope.instance="AllContactsController";
+
+  },
+
+  getContact: function(){
+    this.$scope.contacts = ServerProvider.$get();
+  },
+
+  addNewContact: function(){
+    this.$scope.currentContact = new Contact()
+  },
+
+  openContactDetails: function (contact) {
+    this.$scope.currentContact = new Contact(contact.firstName, contact.lastName, contact.email, contact.phone)
+
+  }
+});
 
 angular.module('contactManager')
-  .controller('AllContactsCtrl', function ($scope) {
-
-  });
+  .controller('AllContactsCtrl', ['$scope', AllContactsController]);
